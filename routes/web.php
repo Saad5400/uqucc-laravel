ea012dfe85896ab0781222c85b0a9716e4475a64
Saad Batwa <86385454+Saad5400@users.noreply.github.com>
2025-12-27 15:47:46 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', [PageController::class, 'home'])->name('home');
 // Tool routes (must come before catch-all route)
 Route::inertia('/adoat/almkafa', 'tools/NextRewardPage')->name('tools.next-reward');
 Route::inertia('/adoat/hasb-alhrman', 'tools/DeprivationCalculatorPage')->name('tools.deprivation-calculator');
-Route::inertia('/adoat/hasb-almaadl', 'tools/GpaCalculatorPage')->name('tools.gpa-calculator');
+Route::inertia('/adwat/hasbh-almadl', 'tools/GpaCalculatorPage')->name('tools.gpa-calculator');
 
 // Catch-all route for content pages (must be last!)
 Route::get('/{slug}', [PageController::class, 'show'])
